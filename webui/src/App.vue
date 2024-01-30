de5367aaad7b9d8f5a60d9d3c07ef61c0020f539
simolillo <Simo02ru@gmail.com>
2024-01-30 14:30:14 +0100

    trying

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -6,10 +6,10 @@ export default {}
 </script>
 
 <template>
-
 	<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
 		<a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#/">Example App</a>
-		<button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+		<button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
+			data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 	</header>
@@ -18,37 +18,47 @@ export default {}
 		<div class="row">
 			<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
 				<div class="position-sticky pt-3 sidebar-sticky">
-					<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+					<h6
+						class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
 						<span>General</span>
 					</h6>
 					<ul class="nav flex-column">
 						<li class="nav-item">
 							<RouterLink to="/" class="nav-link">
-								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#home"/></svg>
+								<svg class="feather">
+									<use href="/feather-sprite-v4.29.0.svg#home" />
+								</svg>
 								Home
 							</RouterLink>
 						</li>
 						<li class="nav-item">
 							<RouterLink to="/link1" class="nav-link">
-								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#layout"/></svg>
+								<svg class="feather">
+									<use href="/feather-sprite-v4.29.0.svg#layout" />
+								</svg>
 								Menu item 1
 							</RouterLink>
 						</li>
 						<li class="nav-item">
 							<RouterLink to="/link2" class="nav-link">
-								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#key"/></svg>
+								<svg class="feather">
+									<use href="/feather-sprite-v4.29.0.svg#key" />
+								</svg>
 								Menu item 2
 							</RouterLink>
 						</li>
 					</ul>
 
-					<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+					<h6
+						class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
 						<span>Secondary menu</span>
 					</h6>
 					<ul class="nav flex-column">
 						<li class="nav-item">
 							<RouterLink :to="'/some/' + 'variable_here' + '/path'" class="nav-link">
-								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#file-text"/></svg>
+								<svg class="feather">
+									<use href="/feather-sprite-v4.29.0.svg#file-text" />
+								</svg>
 								Item 1
 							</RouterLink>
 						</li>
@@ -57,11 +67,30 @@ export default {}
 			</nav>
 
 			<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-				<RouterView />
+				<!-- Conditionally render content based on route -->
+				<router-view v-if="!$route.path.includes('/login')" />
+				<template v-else>
+					<!-- Content for the login page -->
+					<div class="container text-center pt-3 pb-2 border-bottom">
+						<h2>Login</h2>
+					</div>
+
+					<div class="h-75 d-flex align-items-center justify-content-center">
+						<form class="border border-dark p-5 rounded shadow-lg" @submit.prevent="login">
+							<!-- Username input -->
+							<div class="form-outline mb-4">
+								<input type="text" id="login-form" class="form-control"
+									pattern="^[a-zA-Z][a-zA-Z0-9_]{2,32}$" />
+								<label class="form-label" for="login-form">Username</label>
+							</div>
+							<!-- Submit button -->
+							<button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
+						</form>
+					</div>
+				</template>
 			</main>
 		</div>
 	</div>
 </template>
 
-<style>
-</style>
+<style></style>
--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -1,11 +1,12 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import HomeViewC from '../views/LoginView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
 	history: createWebHashHistory(import.meta.env.BASE_URL),
 	routes: [
-		{path: '/', component: HomeViewC},
+		{path: '/', redirect: '/login'},
+		{path: '/login', component: LoginView},
 		{path: '/link1', component: HomeView},
 		{path: '/link2', component: HomeView},
 		{path: '/some/:id/link', component: HomeView},
