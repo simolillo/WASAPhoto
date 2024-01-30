cc462abcb0a8774eab4315a20115bdac9b153cfd
simolillo <Simo02ru@gmail.com>
2024-01-30 18:01:38 +0100

    again

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -1,20 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
 <script>
-	export default {
-		data: function () {
-			return {
-				userId: localStorage.getItem("token"),
-				username: localStorage.getItem("username"),
-			};
-		},
-	};
+export default {}
 </script>
 
 <template>
+
 	<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-		<a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#/">WASA Photo</a>
-		<button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
-			data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
-			aria-label="Toggle navigation">
+		<a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#/">Example App</a>
+		<button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 	</header>
@@ -23,88 +18,41 @@
 		<div class="row">
 			<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
 				<div class="position-sticky pt-3 sidebar-sticky">
-					<div>
-						<h6
-							class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
-							<span>General</span>
-						</h6>
-						<ul class="nav flex-column">
-							<li class="nav-item">
-								<RouterLink :to="{
-										name: 'Stream',
-										params: { userId: this.userId },
-									}" class="nav-link">
-									<svg class="feather">
-										<use href="/feather-sprite-v4.29.0.svg#home" />
-									</svg>
-									Stream
-								</RouterLink>
-							</li>
-							<li class="nav-item">
-								<RouterLink :to="{
-										name: 'UploadPhoto',
-										params: { userId: this.userId },
-									}" class="nav-link">
-									<svg class="feather">
-										<use href="/feather-sprite-v4.29.0.svg#image" />
-									</svg>
-									Upload Photo
-								</RouterLink>
-							</li>
-							<li class="nav-item">
-								<RouterLink :to="{
-										name: 'SearchUser',
-										params: { userId: this.userId },
-									}" class="nav-link">
-									<svg class="feather">
-										<use href="/feather-sprite-v4.29.0.svg#search" />
-									</svg>
-									Search User
-								</RouterLink>
-							</li>
-						</ul>
+					<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+						<span>General</span>
+					</h6>
+					<ul class="nav flex-column">
+						<li class="nav-item">
+							<RouterLink to="/" class="nav-link">
+								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#home"/></svg>
+								Home
+							</RouterLink>
+						</li>
+						<li class="nav-item">
+							<RouterLink to="/link1" class="nav-link">
+								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#layout"/></svg>
+								Menu item 1
+							</RouterLink>
+						</li>
+						<li class="nav-item">
+							<RouterLink to="/link2" class="nav-link">
+								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#key"/></svg>
+								Menu item 2
+							</RouterLink>
+						</li>
+					</ul>
 
-						<h6
-							class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
-							<span>My Account</span>
-						</h6>
-						<ul class="nav flex-column">
-							<li class="nav-item">
-								<!-- <RouterLink
-									:to="{
-										name: 'MyAccount',
-										params: { username: this.username },
-									}"
-									class="nav-link"
-								>
-									<svg class="feather">
-										<use
-											href="/feather-sprite-v4.29.0.svg#user"
-										/>
-									</svg>
-									MyAccount
-								</RouterLink> -->
-							</li>
-							<li class="nav-item">
-								<RouterLink :to="{
-										name: 'SetMyUsername',
-									}" class="nav-link">
-									<svg class="feather">
-										<use href="/feather-sprite-v4.29.0.svg#edit-3" />
-									</svg>
-									Update My Username
-								</RouterLink>
-							</li>
-							<li class="nav-item">
-								<RouterLink :to="{ name: 'Login' }" class="nav-link">
-									<svg class="feather">
-										<use href="/feather-sprite-v4.29.0.svg#log-out" />
-									</svg>
-									Logout
-								</RouterLink>
-							</li>
-						</ul>
-					</div>
+					<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+						<span>Secondary menu</span>
+					</h6>
+					<ul class="nav flex-column">
+						<li class="nav-item">
+							<RouterLink :to="'/some/' + 'variable_here' + '/path'" class="nav-link">
+								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#file-text"/></svg>
+								Item 1
+							</RouterLink>
+						</li>
+					</ul>
 				</div>
 			</nav>
 
@@ -115,4 +63,5 @@
 	</div>
 </template>
 
-<style></style>
+<style>
+</style>
--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -1,29 +1,13 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Login from '../views/LoginView.vue'
-import StreamView from '../views/StreamView.vue'
-import MyAccount from '../views/MyAccount.vue'
-import Followings from '../views/GetFollowings.vue'
-import Banned from '../views/GetBans.vue'
-import Followers from '../views/GetFollowers.vue'
-import UploadPhoto from '../views/UploadPhotoView.vue'
-import SearchUser from '../views/SearchUser.vue'
-import SetMyUsername from '../views/SetMyUsername.vue'
-
 
 const router = createRouter({
 	history: createWebHashHistory(import.meta.env.BASE_URL),
 	routes: [
-		{ path: '/', component: Login, name: 'Login' },
-		{ path: '/users/stream', component: StreamView, name: 'Stream' },
-		{ path: '/users/photos', component: UploadPhoto, name: 'UploadPhoto' },
-		{ path: '/profile/:username', component: MyAccount, name: 'MyAccount'},
-		{ path: '/users/:userId/followings/:username', component: Followings, name: 'Followings'},
-		{ path: '/users/:userId/followers/:username', component: Followers, name: 'Followers'},
-		{ path: '/users/:userId/banned', component: Banned, name: 'Banned'},
-		{ path: '/search', component: SearchUser, name: 'SearchUser'},
-		{ path: '/users/', component: SetMyUsername, name: 'SetMyUsername'}
-		
+		{path: '/', component: HomeView},
+		{path: '/link1', component: HomeView},
+		{path: '/link2', component: HomeView},
+		{path: '/some/:id/link', component: HomeView},
 	]
 })
 
