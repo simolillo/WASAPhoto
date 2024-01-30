e0b8349d33b60b43dce2c73f3f0960246a6bdb21
simolillo <Simo02ru@gmail.com>
2024-01-30 12:43:07 +0100

    inverted

--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -1,6 +1,6 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
-import LoginView from '../views/LoginView.vue'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
 	history: createWebHashHistory(import.meta.env.BASE_URL),
