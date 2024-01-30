cde6d306c378fb97486a4d70a067992cd6560713
simolillo <Simo02ru@gmail.com>
2024-01-30 13:32:04 +0100

    t

--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -1,6 +1,6 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import HomeViewC from '../views/LoginView.vue'
+import HomeViewC from '../views/ToginView.vue'
 
 const router = createRouter({
 	history: createWebHashHistory(import.meta.env.BASE_URL),
