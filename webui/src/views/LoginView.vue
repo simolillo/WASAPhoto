d65a04d675912689d886c9bd4da718331bcbb2bc
simolillo <Simo02ru@gmail.com>
2024-01-30 14:42:03 +0100

    if else log

--- webui/src/views/LoginView.vue
+++ webui/src/views/LoginView.vue
@@ -41,21 +41,21 @@ export default {
 </script>
 
 <template>
-    <!-- <div class="container text-center pt-3 pb-2 border-bottom">
+    <div class="container text-center pt-3 pb-2 border-bottom">
         <h2>Login</h2>
     </div>
 
     <div class="h-75 d-flex align-items-center justify-content-center">
         <form class="border border-dark p-5 rounded shadow-lg" @submit.prevent="login"> -->
             <!-- Username input -->
-            <!-- <div class="form-outline mb-4">
+            <div class="form-outline mb-4">
                 <input type="text" id="login-form" class="form-control" pattern="^[a-zA-Z][a-zA-Z0-9_]{2,32}$" />
                 <label class="form-label" for="login-form">Username</label>
-            </div> -->
+            </div>
             <!-- Submit button -->
-            <!-- <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
+            <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
         </form>
-    </div> -->
+    </div>
 </template>
 
 <style></style>
