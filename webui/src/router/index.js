42544852666a0e0937911f22475588d4633fe1c3
simolillo <Simo02ru@gmail.com>
2024-01-30 17:01:30 +0100

    Update index.js

--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -6,10 +6,9 @@ import StreamView from '../views/StreamView.vue'
 const router = createRouter({
 	history: createWebHashHistory(import.meta.env.BASE_URL),
 	routes: [
-		{path: '/', redirect: '/home'},
+		{path: '/', redirect: '/login'},
 		{path: '/login', component: LoginView},
 		{path: '/stream', component: StreamView},
-		{path: '/home', component: HomeView},
 		{path: '/link1', component: HomeView},
 		{path: '/link2', component: HomeView},
 		{path: '/some/:id/link', component: HomeView},
