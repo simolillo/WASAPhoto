4526d20f587951d15f8c921449e1eb5c2d140cb7
simolillo <simo02ru@gmail.com>
2024-02-10 18:04:35 +0100

    unstoppable

--- webui/src/components/LikeModal.vue
+++ webui/src/components/LikeModal.vue
@@ -1,32 +1,35 @@
 <script>
-export default {	
+export default {
 
 	props:['modalID','likesList'],
 
     methods: {
-        visitAuthorProfile() {
+        visitLiker(username) {
             // /profiles/:username
-			this.$router.push(`/profiles/${this.authorUsername}`);
+            this.$emit('visitLiker', username); // signal to parent
 		},
     }
 }
 </script>
 
 <template>
-	<div class="modal fade modal-dialog modal-dialog-centered modal-dialog-scrollable my-modal-disp-none" :id="modal_id" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog">
+	<div class="modal fade my-modal-disp-none" :id="modalID" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-dialog modal-dialog-scrollable">
             <div class="modal-content">
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" :id="modalID">Likes</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
-                    <div v-for="user in likesList">
-                        <div class="row mb-2 mt-2">
-                            <div class="col d-flex justify-content-center">
-                                <div class="user-mini-card card bg-transparent border-start">
-                                    <div class="card-body">
-                                        <h5 @click="visitAuthorProfile" class="user-mini-card-title d-flex justify-content-center ">@{{ user.userID }}</h5>
+                <div v-for="user in likesList">
+                    <div class="modal-body">
+                        <div class="container-fluid">
+                            <hr>
+                            <div class="row mb-2 mt-2">
+                                <div class="col d-flex justify-content-center">
+                                    <div class="user-mini-card card bg-transparent border-start">
+                                        <div class="card-body">
+                                            <h5 @click="visitLiker(user.username)" class="user-mini-card-title d-flex justify-content-center ">@{{ user.username }}</h5>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -41,5 +44,15 @@ export default {
 <style>
 .my-modal-disp-none{
 	display: none;
+}
+.user-mini-card{
+    color: black;
+    width: 25%;
+    border: none;
+    border-color: black;
+}
+.user-mini-card-title:hover{
+    cursor: pointer;
+    font-weight: bold;
 }
 </style>
